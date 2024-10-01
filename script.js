c993c947378be0c6b321e1e435fefcd07b664bc3
Daniel Andrade <danielandradecv@gmail.com>
2024-10-01 14:58:31 -0500

    primer banner solo esta el fondo de las hojas y naranjas con movimiento mouse y scroll

---
+++ script.js
@@ -0,0 +1,31 @@
+const hojasBG = document.querySelector('.hojasBG-1');
+const naranja = document.querySelector('.naranjas');
+
+let zoomLevel = 1;
+let mouseX = 0;
+let mouseY = 0;
+let opacity = 1;
+
+// Función que actualiza el transform y la opacidad con ambos efectos
+function updateTransform() {
+  hojasBG.style.transform = `translate(${mouseX * -100}px, ${mouseY * -100}px) scale(${zoomLevel})`;
+  hojasBG.style.opacity = opacity;
+
+  naranja.style.transform = `translate(${mouseX * 50}px, ${mouseY * 50}px) scale(${zoomLevel})`;
+  naranja.style.opacity = opacity;
+}
+
+// Efecto de zoom al hacer scroll con desvanecimiento
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY;
+  zoomLevel = 1 + scrollPosition / 500;
+  opacity = 1 - scrollPosition / 500; // Actualiza la opacidad
+  updateTransform();
+});
+
+// Efecto de movimiento del mouse
+document.addEventListener('mousemove', (event) => {
+  mouseX = (event.clientX / window.innerWidth) - 0.5;
+  mouseY = (event.clientY / window.innerHeight) - 0.5;
+  updateTransform();
+});
