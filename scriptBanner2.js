d8ef5f692f3aedb47a1116fd7f8be0603a546a11
Daniel Andrade <danielandradecv@gmail.com>
2024-10-07 17:54:52 -0500

    banner 2 finish suuu

--- scriptBanner2.js
+++ scriptBanner2.js
@@ -3,16 +3,23 @@ document.addEventListener('scroll', function() {
     const banner = document.querySelector('.banner-2');
     const vaso = document.querySelector('.vaso');
     const burbujas = document.querySelector('.svg');
+    const efervescencia = document.querySelector('.efervescencia'); 
+    const efectTitle = document.querySelector('.texto'); 
+
 
     const bannerPosition = banner.getBoundingClientRect().top;
     const windowHeight = window.innerHeight;
 
     // Detectar cuando el banner está en la mitad de la pantalla
     if (bannerPosition < windowHeight / 2 && bannerPosition > 0) {
-        banner.style.backgroundColor = '#ff0000'; // Cambia el fondo a rojo cuando el banner está en la mitad
+         // Cambia el fondo a rojo cuando el banner está en la mitad
         pastilla.classList.add('fall');
         vaso.classList.add('fuel');
         burbujas.classList.add('start');
+        efervescencia.classList.add('start');
+        efectTitle.classList.add('start');
+        efectTitle.classList.add('animate__animated', 'animate__bounce');
+
 
 
 
@@ -23,6 +30,10 @@ document.addEventListener('scroll', function() {
         pastilla.classList.remove('fall'); // Eliminar la clase 'fall' cuando sale de la mitad de la pantalla
         vaso.classList.remove('fuel');
         burbujas.classList.remove('start');
+        efervescencia.classList.remove('start');
+        efectTitle.classList.remove('start')
+        efectTitle.classList.remove('animate__animated', 'animate__bounce');;
+
 
 
     }
